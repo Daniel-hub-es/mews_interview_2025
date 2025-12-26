98414c205436a86fe3459ee8bfda5be0252d4bb6
Daniel-hub-es <Valbuena1v2b@gmail.com>
2025-12-26 18:11:19 +0000

    Adjust calendar

--- mews_project/models/marts/dim/dim__calendar.sql
+++ mews_project/models/marts/dim/dim__calendar.sql
@@ -1,4 +1,7 @@
 select 
 	"Weekday" as weekday,
-	to_date("Day"::varchar, 'DD/MM/YYYY') AS dates
+	to_date("Day"::varchar, 'DD/MM/YYYY') AS dates,
+	"Week_Number" as week_number,
+	"Month" as month,
+	"Year" as year
 from {{ source('external', 'generated_calendar') }}
