96399c8703ad65c66c5ff90e4ebdb9c6a80ea018
Daniel-hub-es <Valbuena1v2b@gmail.com>
2025-12-26 20:38:43 +0000

    Enhance fct popularity model

--- mews_project/models/marts/fct/fct__rate_popularity.sql
+++ mews_project/models/marts/fct/fct__rate_popularity.sql
@@ -4,39 +4,32 @@ with
         select *
         from {{ ref('int__reservations_rates') }}
     ),
-
-    calendar as (
-        select *
-        from {{ ref('dim__calendar') }}
-    ),
     
     final as (
     select
 		-- Year Date
-        created_utc,
-		cal.year as created_year_reservation,
-        cal.month,
-        cal.week_number,
+        extract(year from created_utc) as created_year_reservation,
+        extract(month from created_utc) as created_month_reservation,
+        extract(year from start_utc) as start_year_reservation,
+        extract(month from start_utc) as start_month_reservation,
         -- Dimensions
-        rr.rate_name, 
-        rr.age_group,
-        rr.gender, 
-        rr.nationality_code,
+        rate_name, 
+        age_group,
+        gender, 
+        nationality_code,
         -- Number of records
         count(*) as total_reservations
-    from reservations_rates rr
-	left join calendar cal
-	on cast(rr.created_utc as date) = cal.dates
+    from reservations_rates
     group by
         -- group by clause for the aggregation
         rate_name, 
         age_group,
         gender, 
         nationality_code,
-		year,
-        month,
-        week_number,
-        created_utc
+		created_year_reservation,
+        created_month_reservation,
+        start_year_reservation,
+        start_month_reservation
         -- order records from the greatest to the lowest amount
     order by total_reservations desc
     )
